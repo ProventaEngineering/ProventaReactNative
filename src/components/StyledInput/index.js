09e22ea351f36a9b66a07aa3670120d1fe681bdc
aeonfaux <tonimaristelacarpio@gmail.com>
2019-04-04 13:47:41 +0800

    Added Spinner on Login Request and Auth Error Field

--- src/components/StyledInput/index.js
+++ src/components/StyledInput/index.js
@@ -2,34 +2,34 @@ import React, { Component } from "react";
 import { View, Image, TextInput } from "react-native";
 import ComponentStyle from "./styles";
 
+const images = {
+  visibilityIcon: require('../../assets/login_eye.png'),
+  userIcon: require('../../assets/login_user.png'),
+  passwordIcon: require('../../assets/login_password.png'),
+};
+
 class StyledInput extends Component {
-  renderRightIcon(type, onChangeText, icon, visibilityIcon) {
-    if (type === "password") {
+  renderRightIcon(type, onChangeText) {
+    if (type === "email") {
       return (
         <View style={ComponentStyle.rightIcon}>
-          <Image style={ComponentStyle.icon} source={visibilityIcon} />
+          <Image style={[ComponentStyle.icon, { opacity: 0 }]} source={images.userIcon} />
         </View>
       );
     } else {
       return (
         <View style={ComponentStyle.rightIcon}>
-          <Image style={[ComponentStyle.icon, { opacity: 0 }]} source={icon} />
+          <Image style={[ComponentStyle.icon]} source={images.visibilityIcon} />
         </View>
       );
     }
   }
   render() {
-    const {
-      type,
-      placeholder,
-      onChangeText,
-      icon,
-      visibilityIcon
-    } = this.props;
+    const { type, onChangeText, placeholder } = this.props;
     return (
       <View style={ComponentStyle.searchSection}>
         <View style={ComponentStyle.leftIcon}>
-          <Image style={ComponentStyle.icon} source={icon} />
+          <Image style={ComponentStyle.icon} source={type === 'email' ? images.userIcon : images.passwordIcon} />
         </View>
         <View style={ComponentStyle.centerBorder}>
           <TextInput
@@ -41,9 +41,7 @@ class StyledInput extends Component {
         {this.renderRightIcon(
           type,
           placeholder,
-          onChangeText,
-          visibilityIcon,
-          icon
+          onChangeText
         )}
       </View>
     );
