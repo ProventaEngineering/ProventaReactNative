0208cd561ef5279a0236e4be8909ee26dc15a61d
Ferdinand Mirasol <ferdinand_mirasol@yahoo.com>
2019-06-08 22:29:40 +0800

    - PiggyBack on console to call the Reactotron.log

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -2,3 +2,5 @@ import Reactotron from "reactotron-react-native";
 Reactotron.configure({ host: "192.168.254.100" }) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .connect(); // let's connect!
+
+console.tronlog = Reactotron.log;
